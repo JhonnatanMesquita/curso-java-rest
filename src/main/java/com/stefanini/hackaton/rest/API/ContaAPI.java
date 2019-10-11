545c5590cdc6205fba0f0a3d0591d6ad55247556
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-10-11 15:18:05 -0300

    Refatorando as APIs

--- src/main/java/com/stefanini/hackaton/rest/API/ContaAPI.java
+++ src/main/java/com/stefanini/hackaton/rest/API/ContaAPI.java
@@ -1,35 +1,54 @@
 package com.stefanini.hackaton.rest.API;
 
 import com.stefanini.hackaton.rest.DAO.ContaDAO;
+import com.stefanini.hackaton.rest.DAO.PessoaDAO;
 import com.stefanini.hackaton.rest.entidades.Conta;
+import com.stefanini.hackaton.rest.entidades.Pessoa;
+import com.stefanini.hackaton.rest.exceptions.NegocioException;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/conta")
+@Stateless
 public class ContaAPI {
 
-    ContaDAO contaDAO = new ContaDAO();
+    @Inject
+    ContaDAO contaDAO;
+
+    @Inject
+    PessoaDAO pessoaDAO;
 
     @GET
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Response consultar(){
+    public Response consultar() throws NegocioException {
 
-        return Response.ok(contaDAO.buscarTodos()).build();
+        List<Conta> c = contaDAO.buscarTodos();
+
+        if(c == null || c.isEmpty()){
+            throw new NegocioException("Nenhuma conta foi encontrada!");
+        }
+
+        return Response.status(200).entity(c).build();
 
     }
 
     @GET
     @Path("/{id}")
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Response consultarId(@PathParam("id") Integer id) {
+    public Response consultarId(@PathParam("id") Integer id) throws NegocioException{
 
         Conta c = contaDAO.buscarId(id);
 
-        System.out.println(c);
+        if(c == null){
+            throw new NegocioException("A conta não existe!");
+        }
 
-        return Response.ok(c).build();
+        return Response.status(200).entity(c).build();
     }
 
     @POST
@@ -37,15 +56,76 @@ public class ContaAPI {
     @Produces(MediaType.APPLICATION_JSON)
     public Response inserir(Conta conta){
 
+        conta.setId(null);
+
         contaDAO.salvar(conta);
 
-        return Response.ok(conta).build();
+        return Response.status(201).entity(conta).build();
     }
 
     @DELETE
     @Path("/{id}")
     public Response deletar(@PathParam("id") Integer id){
         contaDAO.deletar(id);
-        return Response.ok("Conta deletada com sucesso!").build();
+        return Response.noContent().build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response alterar(Conta conta, @PathParam("id") Integer id) throws NegocioException{
+        Conta c = contaDAO.buscarId(id);
+
+        if(c == null){
+            throw new NegocioException("Essa conta não foi encontrada!");
+        }
+
+        updateData(conta, c);
+
+        c = contaDAO.atualizar(conta);
+
+        return Response.status(202).entity(c).build();
+    }
+
+    @PUT
+    @Path("/associar/{pessoa}/{conta}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response associar(@PathParam("pessoa") Integer idPessoa, @PathParam("conta") Integer idConta) throws NegocioException{
+
+        Pessoa p = pessoaDAO.buscarId(idPessoa);
+
+        if(p == null){
+            throw new NegocioException("Pessoa não encontrada");
+        }
+
+        Conta c = contaDAO.buscarId(idConta);
+
+        if(c == null){
+            throw new NegocioException("Conta não encontrada");
+        }
+
+        p.setConta(c);
+
+        p = pessoaDAO.atualizar(p);
+
+        return Response.status(202).entity(p).build();
+    }
+
+
+    private void updateData(Conta newObj, Conta obj){
+        if (newObj.getId() == null) {
+            newObj.setId(obj.getId());
+        }
+        if (newObj.getConta() == null) {
+            newObj.setConta(obj.getConta());
+        }
+        if (newObj.getAgencia() == null) {
+            newObj.setAgencia(obj.getAgencia());
+        }
+        if (newObj.getSenha() == null) {
+            newObj.setSenha(obj.getSenha());
+        }
     }
 }
--- src/main/java/com/stefanini/hackaton/rest/API/PessoaAPI.java
+++ src/main/java/com/stefanini/hackaton/rest/API/PessoaAPI.java
@@ -3,43 +3,46 @@ package com.stefanini.hackaton.rest.API;
 import com.stefanini.hackaton.rest.DAO.PessoaDAO;
 import com.stefanini.hackaton.rest.entidades.Pessoa;
 import com.stefanini.hackaton.rest.exceptions.NegocioException;
-import com.stefanini.hackaton.rest.parsers.PessoaParse;
-import com.stefanini.hackaton.rest.persistence.RepositorioPessoa;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/pessoa")
+@Stateless
 public class PessoaAPI {
 
     @Inject
-    RepositorioPessoa repositorioPessoa;
-
-    @Inject
-    PessoaParse pessoaParse;
-
-
-    PessoaDAO pessoaDAO = new PessoaDAO();
+    PessoaDAO pessoaDAO;
 
     @GET
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Response consultar() {
+    public Response consultar() throws NegocioException{
 
-        return Response.ok(pessoaDAO.buscarTodos()).build();
+        List<Pessoa> p = pessoaDAO.buscarTodos();
+
+        if(p == null || p.isEmpty()){
+            throw new NegocioException("Nenhuma pessoa encontrada!");
+        }
+
+        return Response.status(200).entity(p).build();
     }
 
     @GET
     @Path("/{id}")
     @Produces(value = MediaType.APPLICATION_JSON)
-    public Response consultarCpf(@PathParam("id") Integer id){
+    public Response consultarCpf(@PathParam("id") Integer id) throws NegocioException{
 
         Pessoa p = pessoaDAO.buscarId(id);
 
-        System.out.println(p);
+        if(p == null){
+            throw new NegocioException("Essa pessoa não existe!");
+        }
 
-        return Response.ok(p).build();
+        return Response.status(200).entity(p).build();
 
     }
 
@@ -54,14 +57,49 @@ public class PessoaAPI {
 
         pessoaDAO.salvar(pessoa);
 
-        return Response.status(200).entity(pessoa).build();
+        return Response.status(201).entity(pessoa).build();
     }
 
     @DELETE
     @Path("/{id}")
     public Response deletar(@PathParam("id") Integer id){
         pessoaDAO.deletar(id);
-        return Response.ok("Deletado com sucesso").build();
+        return Response.status(204).entity("Deletado com sucesso").build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response alterar(Pessoa pessoa, @PathParam("id") Integer id) throws  NegocioException{
+
+        Pessoa p = pessoaDAO.buscarId(id);
+
+        if(p == null){
+            throw new NegocioException("Essa pessoa não foi encontrada!");
+        }
+
+        updateData(pessoa, p);
+
+        p = pessoaDAO.atualizar(pessoa);
+
+        return Response.status(202).entity(p).build();
+
+    }
+
+    private void updateData(Pessoa newObj, Pessoa obj){
+        if (newObj.getId() == null) {
+            newObj.setId(obj.getId());
+        }
+        if(newObj.getCpf() == null){
+            newObj.setCpf(obj.getCpf());
+        }
+        if(newObj.getNome() == null){
+            newObj.setNome(obj.getNome());
+        }
+        if(newObj.getConta() == null){
+            newObj.setConta(obj.getConta());
+        }
     }
 
 }
